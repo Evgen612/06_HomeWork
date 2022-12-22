3fdad60a65bdc9baa53a0c86cca45b96be569a05
evg_smi <evg_smi@mail.ru>
2022-12-22 23:55:38 +0300

    task43

---
+++ task43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void InterPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / ( k1 - k2);
+    double y = k1 * x + b1;
+    if(k1 == k2 && b1 !=b2) Console.WriteLine("Прямые парралельны, точки пересечения нет");
+    else if(k1 == k2 && b1 ==b2) Console.WriteLine("Прямые полностью совпадают");
+    else Console.WriteLine($"Точка пересечения имеет координаты {x}; {y}");
+}
+
+Console.WriteLine("Введите значение b1, k1, b2, k2, после каждого нажав ВВОД");
+double B1 = double.Parse(Console.ReadLine());
+double K1 = double.Parse(Console.ReadLine());
+double B2 = double.Parse(Console.ReadLine());
+double K2 = double.Parse(Console.ReadLine());
+InterPoint(B1, K1, B2, K2);
